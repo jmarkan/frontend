178441e603fddd1b4eda43629f215c78396152cb
Jayat Markan (jmarkan) <jmarkan@expedia.com>
2019-03-05 15:47:00 -0500

    changed app text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            This is Jay's React project
+            This is Jay's Awesome React project
           </p>
           <a
             className="App-link"
